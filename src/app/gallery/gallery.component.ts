6ac0fa95a8b8064811bc6825c1fd6c69b21e2eb4
Arko Mandal <arkomandal@Arkos-MacBook-Air.local>
2023-10-11 23:03:51 +0530

    changes

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -6,13 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  @ViewChild('gallery', { static: false }) private gallery: ElementRef<HTMLDivElement> | undefined;
+  isTestDivScrolledIntoView: boolean = true;
   height: string = "200";
   width: string = "200";
   items: string[] = [];
   totalItems: number = 49;
 
-  constructor() {
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView() {
+    if (this.gallery) {
+      const rect = this.gallery.nativeElement.getBoundingClientRect();
+      // const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      // this.isTestDivScrolledIntoView = topShown && bottomShown;
+      this.isTestDivScrolledIntoView = !bottomShown;
+    }
+  }
 
+  constructor() {
   }
 
   ngOnInit() {
@@ -22,5 +34,5 @@ export class GalleryComponent implements OnInit {
       i++;
     }
   }
-  
+
 }
